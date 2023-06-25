fd319a9bdb3cc2d878f18feeb25a73eb2a193dc2
guidobonerz <guido.bonerz@drazil.de>
2023-06-25 12:47:35 +0200

    add logging in tool windows console view

--- src/main/java/de/drazil/ptah/GeneratorRunner.java
+++ src/main/java/de/drazil/ptah/GeneratorRunner.java
@@ -2,9 +2,14 @@ package de.drazil.ptah;
 
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.process.ProcessOutput;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.execution.util.ExecUtil;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.content.Content;
 import de.drazil.ptah.settings.PtahProjectSettings;
 import de.drazil.ptah.settings.PtahProjectSettingsProvider;
 
@@ -17,6 +22,11 @@ public class GeneratorRunner {
         PtahProjectSettings settings = provider.getState();
         String ptahExecutable = settings.pathToGeneratorExecutable;
         String configFile = String.format("%s/%s", project.getBasePath(), settings.pathToConfigFile);
+        ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Code Generator");
+
+        Content content = toolWindow.getContentManager().findContent("Path Console");
+        ConsoleView consoleView = (ConsoleView) content.getComponent();
+        consoleView.clear();
         try {
             GeneralCommandLine command = new GeneralCommandLine();
             command.setExePath(ptahExecutable);
@@ -29,18 +39,23 @@ public class GeneratorRunner {
             if (settings.purgeOutFolders) {
                 command.addParameter("-p");
             }
-
             ProcessOutput output = ExecUtil.execAndGetOutput(command);
             if (output.getExitCode() == 0) {
                 com.intellij.openapi.vfs.VirtualFileManager.getInstance().syncRefresh();
-                PtahNotifier.notify(project, "Code generation successful:"+output.getStdout(), com.intellij.notification.NotificationType.INFORMATION);
+                consoleView.print("Code generation successful\n", ConsoleViewContentType.NORMAL_OUTPUT);
+                consoleView.print(output.getStdout(), ConsoleViewContentType.NORMAL_OUTPUT);
+                PtahNotifier.notify(project, "Code generation successful, see console in tool window", com.intellij.notification.NotificationType.INFORMATION);
             } else {
-                PtahNotifier.notify(project, "Code generation unsuccessful:"+output.getStderr(), com.intellij.notification.NotificationType.ERROR);
+                consoleView.print("Code generation unsuccessful\n", ConsoleViewContentType.NORMAL_OUTPUT);
+                consoleView.print(output.getStderr(), ConsoleViewContentType.NORMAL_OUTPUT);
+                PtahNotifier.notify(project, "Code generation unsuccessful, see console in tool window", com.intellij.notification.NotificationType.ERROR);
             }
 
 
         } catch (Exception ex) {
-            PtahNotifier.notify(project, "Code generation unsuccessful:" + ex.getMessage(), com.intellij.notification.NotificationType.ERROR);
+            consoleView.print("Code generation unsuccessful\n", ConsoleViewContentType.NORMAL_OUTPUT);
+            consoleView.print(ex.getMessage(), ConsoleViewContentType.NORMAL_OUTPUT);
+            PtahNotifier.notify(project, "Code generation unsuccessful, see console in tool window", com.intellij.notification.NotificationType.ERROR);
             ex.printStackTrace();
         }
     }
--- src/main/java/de/drazil/ptah/PtahTool.java
+++ src/main/java/de/drazil/ptah/PtahTool.java
@@ -1,12 +1,15 @@
 package de.drazil.ptah;
 
+import com.intellij.execution.filters.TextConsoleBuilderFactory;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.ide.DataManager;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.util.ui.FormBuilder;
+import com.intellij.ui.content.Content;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -17,17 +20,28 @@ public class PtahTool implements ToolWindowFactory {
     private JPanel mainPanel;
     private JButton startGeneratorButton;
 
+    private ConsoleView consoleView;
+
     @Override
 
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
 
+        consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
+        Content content = toolWindow.getContentManager().getFactory().createContent(consoleView.getComponent(), "Path Console", false);
+        toolWindow.getContentManager().addContent(content);
+        consoleView.print("Ptah has started", ConsoleViewContentType.NORMAL_OUTPUT);
+
+
+        /*
         mainPanel = FormBuilder.createFormBuilder()
                 .addComponent(getStartGeneratorButton(), 0)
                 .addComponentFillVertically(new JPanel(), 0)
                 .getPanel();
         toolWindow.getComponent().add(mainPanel);
-
+*/
     }
+
+
 
     private JButton getStartGeneratorButton() {
         if (startGeneratorButton == null) {
